00f9a253a506ed68afba25b1bb972a5b4a91751f
Vinicius Pietscher <vini.914@gmail.com>
2019-09-05 20:04:51 -0400

    Fix test for downcase email.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,8 +55,10 @@ class UserTest < ActiveSupport::TestCase
     assert_not duplicate_user.valid?
   end
 
-  test "email shold be downcase" do
-    @user.email.downcase
-    assert @user.valid?
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
   end
 end
